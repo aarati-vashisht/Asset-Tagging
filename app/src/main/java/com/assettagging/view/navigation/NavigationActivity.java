ba1a1c69f63b983d50de413cb71565c8df8cf611
aarati_vashisht <aarati.vashisht2804@gmail.com>
2018-12-28 10:11:15 +0530

    asdfghj

--- app/src/main/java/com/assettagging/view/navigation/NavigationActivity.java
+++ app/src/main/java/com/assettagging/view/navigation/NavigationActivity.java
@@ -127,6 +127,7 @@ public class NavigationActivity extends BaseActivity
         }
 
         onRefreshListener();
+        /////////////////////////////
     }
 
     public void getDataFromDatabase() {
